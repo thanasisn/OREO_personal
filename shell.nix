e8e184851ed8ec9a2e79607ab75ade0f3af13f79
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-04-05 16:14:29 +0300

    Commit 2025-04-05 16:14

--- shell.nix
+++ shell.nix
@@ -6,13 +6,15 @@ let
   pkgs = import <nixpkgs> {};
   pythonPkgs = pkgs.python313.withPackages (ps: with ps; [
     numpy
+    xarray
+    #netCDF4
   ]);
 
 in pkgs.mkShell rec {
-  name = "myPython";
+  name = "OREOenv";
   buildInputs = with pkgs; [ pythonPkgs ];
-  # shellHook = ''
-  #   export PS1='\n\[\033[1;34m\][${name}@\h:\w]\$\[\033[0m\] '
-  # '';
+  shellHook = ''
+    export PS1='\n\[\033[1;34m\][${name}@\h:\w]\$\[\033[0m\] '
+  '';
 }
 
